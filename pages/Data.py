feb6fb86ddcf82ccb4d6fcb1c517390251b91fea
martinasp00 <150537935+martinasp00@users.noreply.github.com>
2024-07-11 14:36:20 +0200

    Update Data.py

--- pages/Data.py
+++ pages/Data.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_csv('laptop_data.csv', index_col=0)
+df = pd.read_csv('csv/laptop_data.csv', index_col=0)
 # df['Price'] = df['Price'].apply(lambda x: 0.0110699 * x)
 df.rename(columns={'Price':'Price(₹)'}, inplace=True)  
 
@@ -9,7 +9,7 @@ st.title("Original Dataset")
 st.dataframe(df, use_container_width=True)
 
 
-df_ = pd.read_csv('preprocesseddata.csv', index_col=0)
+df_ = pd.read_csv('csv/preprocesseddata.csv', index_col=0)
 
 st.title("Preprocessed Dataset")
 st.dataframe(df_, use_container_width=True)
