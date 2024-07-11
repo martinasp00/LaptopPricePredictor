97e20becb4a9de1eb455128cfa7a13529a360a9b
martinasp00 <150537935+martinasp00@users.noreply.github.com>
2024-07-11 14:15:23 +0200

    Update Data.py

--- pages/Data.py
+++ pages/Data.py
@@ -3,12 +3,13 @@ import pandas as pd
 
 df = pd.read_csv('laptop_data.csv', index_col=0)
 df['Price'] = df['Price'].apply(lambda x: 0.0110699 * x)
+df.rename(columns={'Price':'Price(₹)'}, inplace=True) 
 
 st.title("Original Dataset")
 st.dataframe(df, use_container_width=True)
 
 
-df_ = pd.read_csv('preprocesseddata.csv')
+df_ = pd.read_csv('preprocesseddata.csv', index_col=0)
 
-st.title("Preprocessed (Training) Dataset")
+st.title("Preprocessed Dataset")
 st.dataframe(df_, use_container_width=True)
